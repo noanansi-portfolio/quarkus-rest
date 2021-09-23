46fc7ffa6170ec27f724be82d8c9492c6177cf60
Felipe Miranda <felip.simi@gmail.com>
2021-09-22 23:29:33 -0300

    API: get all greetings

--- src/main/java/com/noanansi/GreetingResource.java
+++ src/main/java/com/noanansi/GreetingResource.java
@@ -1,6 +1,10 @@
 package com.noanansi;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -8,9 +12,12 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
-    }
+  private static final HashMap<UUID, String> greetings = new HashMap<>();
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public HashMap<UUID, String> listAll() {
+    return greetings;
+  }
+
 }
