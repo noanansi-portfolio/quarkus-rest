9e7d4c092d808e134222be6616fb3050a75d132b
Felipe Miranda <felip.simi@gmail.com>
2021-09-24 02:01:32 -0300

    API: get greeting by id

--- src/main/java/com/noanansi/GreetingResource.java
+++ src/main/java/com/noanansi/GreetingResource.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
@@ -18,6 +19,21 @@ public class GreetingResource {
   @Produces(MediaType.APPLICATION_JSON)
   public HashMap<UUID, String> listAll() {
     return greetings;
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getById(@PathParam("id") final String id) {
+    final var message = greetings.get(UUID.fromString(id));
+    if (message == null) {
+      return Response
+          .status(Response.Status.NOT_FOUND)
+          .build();
+    }
+    return Response
+        .ok(Map.of("id", id, "message", message))
+        .build();
   }
 
 }
--- src/test/java/com/noanansi/GreetingResourceTest.java
+++ src/test/java/com/noanansi/GreetingResourceTest.java
@@ -1,6 +1,7 @@
 package com.noanansi;
 
 import io.quarkus.test.junit.QuarkusTest;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,13 +10,20 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+  @Test
+  public void givenNoPathVariable_whenGettingResource_thenOk() {
+    given()
+        .when().get("/hello")
+        .then()
+        .statusCode(200);
+  }
+
+  @Test
+  public void givenNonExistingId_whenGettingResource_thenNotFound() {
+    given()
+        .when().get("/hello/" + UUID.randomUUID())
+        .then()
+        .statusCode(404);
+  }
 
 }
